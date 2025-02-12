8db15f02c56de3e517a2ce6a13c03c849dbe2aa7
Robotics Lab 229R <roboticslab229r@csc229-r.ca>
2025-02-11 18:38:09 -0700

    some implementation

---
+++ bag_analysis/plot_bag.py
@@ -0,0 +1,61 @@
+import sys
+import rosbag
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot(filename, robot_name):
+    bag = rosbag.Bag(filename)
+    messages = list(bag.read_messages(topics=[f"/{robot_name}/wheels_driver_node/wheels_cmd"]))
+    radius = 0.0318
+    baseline = 0.1
+
+    xi_world_frame = np.array([[0], [0], [np.pi/2]])
+    theta = np.pi/2
+
+    x_world_values = []
+    y_world_values = []
+    theta_world_values = []
+
+    for i, bag_message in enumerate(messages):
+        if i == len(messages)-1:
+            break
+        msg = bag_message.message
+        next_msg = messages[i+1].message
+
+        current_stamp = msg.header.stamp
+        next_stamp = next_msg.header.stamp
+        dt = (next_stamp - current_stamp).to_sec()
+        
+        change_robot_frame = np.array([[(radius * msg.vel_right)/2 + (radius * msg.vel_left)/2], 
+                                        [0], 
+                                        [(radius * msg.vel_right)/baseline + (radius * msg.vel_left)/baseline]])
+        theta += change_robot_frame[2]*dt
+        
+        change_in_xi_world_frame = np.array([[np.cos(theta)[0], -np.sin(theta)[0], 0], 
+                                       [np.sin(theta)[0], np.cos(theta)[0], 0], 
+                                       [0, 0, 1]]) @ change_robot_frame
+        
+        xi_world_frame += change_in_xi_world_frame*dt
+        print(xi_world_frame[0, 0]*1e+3, xi_world_frame[1, 0]*1e+3)
+        
+        x_world_values.append(xi_world_frame[0, 0])
+        y_world_values.append(xi_world_frame[1, 0])
+        theta_world_values.append(xi_world_frame[2, 0])
+
+
+    bag.close()
+    plt.figure(figsize=(8, 6))
+    plt.plot(x_world_values, y_world_values, label="Robot Path", marker="o", markersize=3, linestyle="-")
+    plt.xlabel("X Position (m)")
+    plt.ylabel("Y Position (m)")
+    plt.title("Robot Trajectory in World Frame")
+    plt.legend()
+    plt.grid()
+    plt.axis("equal")  # Ensure equal scaling for x and y
+    plt.show()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 4 or len(sys.argv) < 3:
+        raise ValueError("Please the rosbag file and the ROBOT_NAME")
+    plot(sys.argv[1], sys.argv[2])
