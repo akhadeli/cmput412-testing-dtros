7ed0321df929db7756df26e32b1c58d32c4f26e4
irl <roboticslab229r@csc229-r.ca>
2025-03-03 19:28:55 -0700

    lane following proportional controller

--- packages/my_package/src/lane_following.py
+++ packages/my_package/src/lane_following.py
@@ -6,6 +6,7 @@ import numpy as np
 import rospy
 from duckietown.dtros import DTROS, NodeType
 from sensor_msgs.msg import CompressedImage
+from duckietown_msgs.msg import WheelsCmdStamped
 
 import cv2
 from cv_bridge import CvBridge
@@ -18,13 +19,19 @@ class LaneFollowing(DTROS):
         # static parameters
         self._vehicle_name = os.environ['VEHICLE_NAME']
         self._camera_topic = f"/{self._vehicle_name}/camera_node/undistorted_image/compressed"
+        wheels_topic = f"/{self._vehicle_name}/wheels_driver_node/wheels_cmd"
+        self._publisher = rospy.Publisher(wheels_topic, WheelsCmdStamped, queue_size=1)
         # bridge between OpenCV and ROS
         self._bridge = CvBridge()
         
         # construct subscriber
         self.sub = rospy.Subscriber(self._camera_topic, CompressedImage, self.callback)
 
-        self.proportional_gain = 0.00001
+        self.proportional_gain = 0.0000002
+        # self.proportional_gain = 0
+        self.vel = 0.3
+
+        self._error = 1
 
     def callback(self, msg):
         # convert JPEG bytes to CV image
@@ -42,9 +49,9 @@ class LaneFollowing(DTROS):
 
         dst = np.float32([
             [100, 382],
-            [100, -400],  # Bottom left (destination for left lane)
+            [100, 0],  # Bottom left (destination for left lane)
             [489, 382],  # Bottom right (destination for right lane)
-            [489, -400],    # Top left (destination after warping)
+            [489, 0],    # Top left (destination after warping)
         ])
 
         # cv2.circle(image, tuple(point), 5, (0, 0, 255), -1)  # Red dots
@@ -66,19 +73,32 @@ class LaneFollowing(DTROS):
         upper_yellow = np.array([35, 255, 255], dtype=np.uint8)
         mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
-        # Display all images in separate windows
-        cv2.imshow("Before (Original Image)", image)  # Original image with source points
-        cv2.imshow("After (Warped Image)", warped)    # Warped image
-        cv2.imshow("White Lane Detection", mask_white)  # White mask
-        cv2.imshow("Yellow Lane Detection", mask_yellow)  # Yellow mask
+        self._error = self.compute_error(mask_yellow=mask_yellow, target_x=100, pixel_value=1) + self.compute_error(mask_yellow=mask_white, target_x=489)
+        print(self._error)
 
-        # print(self.compute_error(mask_yellow=mask_yellow, target_x=100))
-        bias = -569301
-        print(self.compute_error(mask_yellow=mask_yellow, target_x=100) + self.compute_error(mask_yellow=mask_white, target_x=489) + bias)
-
-        cv2.waitKey(1)
+        # # Display all images in separate windows
+        # cv2.imshow("Before (Original Image)", image)  # Original image with source points
+        # cv2.imshow("After (Warped Image)", warped)    # Warped image
+        # # cv2.imshow("White Lane Detection", mask_white)  # White mask
+        # # cv2.imshow("Yellow Lane Detection", mask_yellow)  # Yellow mask
+        # cv2.waitKey(1)
     
-    def compute_error(self, mask_yellow, target_x=100):
+    def run(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            if self._error < 0:
+                message = WheelsCmdStamped(vel_left=self.vel, vel_right=self.vel+abs(self._error)*self.proportional_gain)
+            elif self._error > 0:
+                message = WheelsCmdStamped(vel_left=self.vel+abs(self._error)*self.proportional_gain, vel_right=self.vel)
+            else:
+                message = WheelsCmdStamped(vel_left=self.vel, vel_right=self.vel)
+            self._publisher.publish(message)
+            rate.sleep()
+
+        
+        # cv2.waitKey(1)
+    
+    def compute_error(self, mask_yellow, target_x=100, pixel_value=1):
         """
         Computes the sum of errors between detected yellow lane pixels and the expected lane position at x=100.
         
@@ -96,13 +116,18 @@ class LaneFollowing(DTROS):
             return 0  # No detected yellow pixels, return 0 error
 
         # Compute the error as the difference from target_x
-        errors = x_coords - target_x
+        errors = (x_coords - target_x) * pixel_value
 
         # Return the sum of the errors
         return np.sum(errors)
+    
+    def on_shutdown(self):
+        stop = WheelsCmdStamped(vel_left=0, vel_right=0)
+        self._publisher.publish(stop)
 
 if __name__ == '__main__':
     # create the node
     node = LaneFollowing(node_name='lane_following')
+    node.run()
     # keep spinning
     rospy.spin()
